a031c6fd1ff92e21b7a187b345e2137bad5e5bff
vishalvenkat12 <vishalvenkat1206@gmail.com>
2023-02-07 16:47:30 +0530

    class commit

--- GIT/dom.js
+++ GIT/dom.js
@@ -1,2 +1,5 @@
-var header=document.getElementById('main-header');
-header.style.borderBottom='solid 3px #000';
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
