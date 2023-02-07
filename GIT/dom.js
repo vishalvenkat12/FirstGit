ed8116ece91b85684b6c1285e6a55733a655355b
vishalvenkat12 <vishalvenkat1206@gmail.com>
2023-02-07 16:56:30 +0530

    Tag name

--- GIT/dom.js
+++ GIT/dom.js
@@ -3,3 +3,6 @@ items[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
+
+var line=document.getElementsByTagName('li');
+line[4].style.backgroundColor='blue';
