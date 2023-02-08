1340aa75764d82357469507e5a474da6f03fa824
vishalvenkat12 <vishalvenkat1206@gmail.com>
2023-02-08 12:44:37 +0530

    EditandDelete

---
+++ GIT/editAndDelete/main.js
@@ -0,0 +1,17 @@
+var form=document.getElementById('addForm');
+var itemList=document.getElementById('items');
+form.addEventListener('submit', addItem);
+function addItem(e){
+e.preventDefault();
+var newItem=document.getElementById('item').value;
+var li=document.createElement('li');
+li.className='list-group-item';
+li.appendChild(document.createTextNode(newItem));
+var deleteBtn =document.createElement('button');
+deleteBtn.className='btn btn-danger btn-sm float-right delete';
+deleteBtn.appendChild(document.createTextNode('x'));
+li.appendChild(deleteBtn);
+
+itemList.appendChild(li);
+
+}
